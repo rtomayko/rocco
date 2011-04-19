3ebbb82f14196f7b0b70f79a29c28d369612968c
Mike West <mike@mikewest.org>
2011-04-19 19:54:42 +0200

    One more test.

--- test/test_docblock_annotations.rb
+++ test/test_docblock_annotations.rb
@@ -1,13 +1,22 @@
 require File.expand_path('../helper', __FILE__)
 
 class RoccoDocblockAnnotationsTest < Test::Unit::TestCase
-  def test_docblock_annotation_conversion
+  def test_basics
     r = Rocco.new( 'test', '', { :language => "c", :docblocks => true } ) { "" } # Generate throwaway instance so I can test `parse`
     assert_equal(
       [
         "Comment\n\n> **param** mixed foo  \n> **return** void  "
       ],
       r.docblock( ["Comment\n\n@param mixed foo\n@return void"] )
+    )
+  end
+  def test_highlighted_in_blocks
+    r = Rocco.new( 'test', '', { :language => "c", :docblocks => true } ) { "" } # Generate throwaway instance so I can test `parse`
+    highlighted = r.highlight( r.split( r.parse( "/**\n * Comment\n * @param type name\n */\ndef codeblock\nend\n" ) ) )
+
+    assert_equal(
+      "<p>Comment</p>\n\n<blockquote><p><strong>param</strong> type name</p></blockquote>\n",
+      highlighted[0][0]
     )
   end
 end
