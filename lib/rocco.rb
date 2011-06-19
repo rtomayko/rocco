618e696bd77b06102b6c662ac182ca17abe662ac
Ryan Tomayko <rtomayko@gmail.com>
2011-06-19 06:44:44 -0700

    move comment styles out into their own module
    Something here was screwing with the doc generation. It's kind of
    noisy in the main codebase anyway.

--- lib/rocco.rb
+++ lib/rocco.rb
@@ -206,57 +206,8 @@ class Rocco
   #
   # At the moment, we're only returning `:single`.  Consider this
   # groundwork for block comment parsing.
-  C_STYLE_COMMENTS = {
-    :single => "//",
-    :multi  => { :start => "/**", :middle => "*", :end => "*/" },
-    :heredoc => nil
-  }
-  COMMENT_STYLES  = {
-    "bash"          =>  { :single => "#", :multi => nil },
-    "c"             =>  C_STYLE_COMMENTS,
-    "coffee-script" =>  {
-      :single => "#",
-      :multi  => { :start => "###", :middle => nil, :end => "###" },
-      :heredoc => nil
-    },
-    "cpp" =>  C_STYLE_COMMENTS,
-    "csharp" => C_STYLE_COMMENTS,
-    "css"           =>  {
-      :single => nil,
-      :multi  => { :start => "/**", :middle => "*", :end => "*/" },
-      :heredoc => nil
-    },
-    "html"           =>  {
-      :single => nil,
-      :multi => { :start => '<!--', :middle => nil, :end => '-->' },
-      :heredoc => nil
-    },
-    "java"          =>  C_STYLE_COMMENTS,
-    "js"            =>  C_STYLE_COMMENTS,
-    "lua"           =>  {
-      :single => "--",
-      :multi => nil,
-      :heredoc => nil
-    },
-    "php" => C_STYLE_COMMENTS,
-    "python"        =>  {
-      :single => "#",
-      :multi  => { :start => '"""', :middle => nil, :end => '"""' },
-      :heredoc => nil
-    },
-    "rb"            =>  {
-      :single => "#",
-      :multi  => { :start => '=begin', :middle => nil, :end => '=end' },
-      :heredoc => "<<-"
-    },
-    "scala"         =>  C_STYLE_COMMENTS,
-    "scheme"        =>  { :single => ";;",  :multi => nil, :heredoc => nil },
-    "xml"           =>  {
-      :single => nil,
-      :multi => { :start => '<!--', :middle => nil, :end => '-->' },
-      :heredoc => nil
-    },
-  }
+  require 'rocco/comment_styles'
+  include CommentStyles
 
   def generate_comment_chars
     @_commentchar ||=
---
+++ lib/rocco/comment_styles.rb
@@ -0,0 +1,56 @@
+class Rocco
+  module CommentStyles
+    C_STYLE_COMMENTS = {
+      :single => "//",
+      :multi  => { :start => "/**", :middle => "*", :end => "*/" },
+      :heredoc => nil
+    }
+
+    COMMENT_STYLES  = {
+      "bash"          =>  { :single => "#", :multi => nil },
+      "c"             =>  C_STYLE_COMMENTS,
+      "coffee-script" =>  {
+        :single => "#",
+        :multi  => { :start => "###", :middle => nil, :end => "###" },
+        :heredoc => nil
+      },
+      "cpp" =>  C_STYLE_COMMENTS,
+      "csharp" => C_STYLE_COMMENTS,
+      "css"           =>  {
+        :single => nil,
+        :multi  => { :start => "/**", :middle => "*", :end => "*/" },
+        :heredoc => nil
+      },
+      "html"           =>  {
+        :single => nil,
+        :multi => { :start => '<!--', :middle => nil, :end => '-->' },
+        :heredoc => nil
+      },
+      "java"          =>  C_STYLE_COMMENTS,
+      "js"            =>  C_STYLE_COMMENTS,
+      "lua"           =>  {
+        :single => "--",
+        :multi => nil,
+        :heredoc => nil
+      },
+      "php" => C_STYLE_COMMENTS,
+      "python"        =>  {
+        :single => "#",
+        :multi  => { :start => '"""', :middle => nil, :end => '"""' },
+        :heredoc => nil
+      },
+      "rb"            =>  {
+        :single => "#",
+        :multi  => { :start => '=begin', :middle => nil, :end => '=end' },
+        :heredoc => "<<-"
+      },
+      "scala"         =>  C_STYLE_COMMENTS,
+      "scheme"        =>  { :single => ";;",  :multi => nil, :heredoc => nil },
+      "xml"           =>  {
+        :single => nil,
+        :multi => { :start => '<!--', :middle => nil, :end => '-->' },
+        :heredoc => nil
+      },
+    }
+  end
+end
